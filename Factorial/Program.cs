eda5792b81c20a7428ae73d971775ff80d7468bf
Ivan <firstclass37@mail.ru>
2016-05-02 20:50:02 +0300

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+
+            if (int.TryParse(Console.ReadLine(), out input))
+            {
+                int result = 1;
+                for (int i = 1; i <= input; i++)
+                    result *= i;
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
